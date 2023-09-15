4f4700c23f8c8ff11ff1569003bc81a7ca3a9d2f
Cristiano Marques Silva <118139423+Cristisiuuu@users.noreply.github.com>
2023-09-15 15:11:16 -0300

    Add files via upload

---
+++ CoordenadasDeUmPonto(1041).c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+	double x, y;
+	
+	scanf("%lf %lf", &x, &y);
+	
+	if(x==0 && y==0){
+		printf("Origem\n");
+	}
+	if(x>0 && y>0){
+		printf("Q1\n");
+	}
+	if(x>0 && y<0){
+		printf("Q4\n");
+	}
+	if(x<0 && y>0){
+		printf("Q2\n");
+	}
+	if(x<0 && y<0){
+		printf("Q3\n");
+	}
+	
+	return 0;
+}
